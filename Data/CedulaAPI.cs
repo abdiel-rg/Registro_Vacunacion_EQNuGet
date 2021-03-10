863c9d328e123e1370bd4ecc5a1c04cb741227ac
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-03-09 23:52:58 -0400

    Add Cedula API

---
+++ Data/CedulaAPI.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Registro_Vacunacion_EQNuGet.Data
+{
+    public class CedulaAPI
+    {
+        [JsonPropertyName("Cedula")]
+        public string Cedula { get; set; }
+
+        [JsonPropertyName("Nombres")]
+        public string Nombres { get; set; }
+
+        [JsonPropertyName("Apellido1")]
+        public string Apellido1 { get; set; }
+
+        [JsonPropertyName("Apellido2")]
+        public string Apellido2 { get; set; }
+
+        [JsonPropertyName("FechaNacimiento")]
+        public string FechaNacimiento { get; set; }
+
+        [JsonPropertyName("LugarNacimiento")]
+        public string LugarNacimiento { get; set; }
+
+        [JsonPropertyName("IDCategoria")]
+        public int IDCategoria { get; set; }
+
+        [JsonPropertyName("IdSexo")]
+        public string IdSexo { get; set; }
+
+        [JsonPropertyName("IdEstadoCivil")]
+        public string IdEstadoCivil { get; set; }
+
+        [JsonPropertyName("IdOcupacion")]
+        public int IdOcupacion { get; set; }
+
+        [JsonPropertyName("IDNacionalidad")]
+        public int IDNacionalidad { get; set; }
+
+        [JsonPropertyName("IDMunicipio")]
+        public int IDMunicipio { get; set; }
+
+        [JsonPropertyName("IDColegio")]
+        public int IDColegio { get; set; }
+
+        [JsonPropertyName("IDCausaCancelacion")]
+        public object IDCausaCancelacion { get; set; }
+
+        [JsonPropertyName("IDEstatus")]
+        public string IDEstatus { get; set; }
+
+        [JsonPropertyName("EstatusCedulaAzul")]
+        public string EstatusCedulaAzul { get; set; }
+
+        [JsonPropertyName("CedulaAnterior")]
+        public string CedulaAnterior { get; set; }
+
+        [JsonPropertyName("mun_ced")]
+        public string MunCed { get; set; }
+
+        [JsonPropertyName("seq_ced")]
+        public string SeqCed { get; set; }
+
+        [JsonPropertyName("ver_ced")]
+        public string VerCed { get; set; }
+
+        [JsonPropertyName("V2004")]
+        public int V2004 { get; set; }
+
+        [JsonPropertyName("V2008")]
+        public int V2008 { get; set; }
+
+        [JsonPropertyName("V2012")]
+        public int V2012 { get; set; }
+
+        [JsonPropertyName("V2016")]
+        public int V2016 { get; set; }
+
+        [JsonPropertyName("PLD")]
+        public int PLD { get; set; }
+
+        [JsonPropertyName("PRD")]
+        public int PRD { get; set; }
+
+        [JsonPropertyName("PRSC")]
+        public int PRSC { get; set; }
+
+        [JsonPropertyName("PRM")]
+        public int PRM { get; set; }
+
+        [JsonPropertyName("Multiplicador_NombreCompleto")]
+        public object MultiplicadorNombreCompleto { get; set; }
+
+        [JsonPropertyName("Multiplicador_Cedula")]
+        public object MultiplicadorCedula { get; set; }
+
+        [JsonPropertyName("Simpatia_Descripcion")]
+        public object SimpatiaDescripcion { get; set; }
+
+        [JsonPropertyName("Ilocalizable")]
+        public object Ilocalizable { get; set; }
+
+        [JsonPropertyName("Apodo")]
+        public object Apodo { get; set; }
+
+        [JsonPropertyName("PadronLF")]
+        public object PadronLF { get; set; }
+
+        [JsonPropertyName("ok")]
+        public bool Ok { get; set; }
+
+        [JsonPropertyName("foto")]
+        public string Foto { get; set; }
+    }
+}
