177164e62e25005553da654cb4b6b6fba89ff5f0
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-03-09 23:55:22 -0400

    Add some DataAnnotations for Personas class

--- Models/Personas.cs
+++ Models/Personas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,17 +10,40 @@ namespace Registro_Vacunacion_EQNuGet.Models
 {
     public partial class Personas
     {
+        [Required]
+        [MinLength(11)]
+        [MaxLength(11)]
         public string Cedula { get; set; }
+
+        [Required]
         public string Nombre { get; set; }
+
+        [Required]
         public string Apellido { get; set; }
+
+        [Phone]
+        [Required]
         public string Telefono { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime FechaNacimiento { get; set; }
+
+        [Required]
         public int VacunaRecibida { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime FechaPrimeraDosis { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime? FechaSegundaDosis { get; set; }
+
+        [Required]
         public int Provincia { get; set; }
 
         public virtual Provincias ProvinciaNavigation { get; set; }
+
         public virtual Vacunas VacunaRecibidaNavigation { get; set; }
     }
 }
